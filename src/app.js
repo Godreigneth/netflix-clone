4387cfa367f04bf8a96403162b92945fc1b2b31f
I Am Levi <reign4all@gmail.com>
2021-02-16 23:16:55 +0100

    Done building the Jumbotron

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from "react";
+
+import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return (
-    <p>Hello</p>
-  );
+  return <JumbotronContainer />;
 }
-
