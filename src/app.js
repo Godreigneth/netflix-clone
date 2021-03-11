6af0d915a6075bc1b801a1856ee9652ba320fd3b
Godreigneth <godreignethlevidibia@yahoo.com>
2021-03-11 17:27:42 +0000

    Created and styled the OptForm component and linked to the Accordion component

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { JumbotronContainer } from "./containers/jumbotron";
 import { FaqsContainer } from "./containers/faqs";
 import { FooterContainer } from "./containers/footer";
 
+
 export default function App() {
     return (
         <>
